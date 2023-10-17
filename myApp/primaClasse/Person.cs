96a0298a7441e8b6939baceda6579cdec22c43d3
WassimCode <wassbcr@gmail.com>
2023-10-17 09:33:58 +0200

    first commit, import classes and created references

---
+++ myApp/primaClasse/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primaClasse
+{
+    internal class Person
+    {
+        //CodiceFiscaleUtility.CodiceFiscale c1 = new CodiceFiscaleUtility.CodiceFiscale("Bachar", "Wassim", "M", DateTime);
+
+    }
+}
